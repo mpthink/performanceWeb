b317b820c0ac9f70865376a67653e73c6a42cb6c
Ma <Paul.P.Ma@emc.com>
2017-11-14 10:19:33 +0800

    update memory select time

--- src/main/webapp/WEB-INF/static/my/vtas/memory.js
+++ src/main/webapp/WEB-INF/static/my/vtas/memory.js
@@ -17,11 +17,10 @@ $(function(){
       {
 		 locale: {format: 'YYYY-MM-DD HH:mm:ss' },
         ranges   : {
-          'Today'       : [moment(), moment()],
-          'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+          'Last 24 Hours'   : [moment().subtract(1, 'days'), moment()],
+          'Last 3 Days' : [moment().subtract(3, 'days'), moment()],
           'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
           'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-          'This Month'  : [moment().startOf('month'), moment().endOf('month')],
           'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
         },
         startDate: moment().subtract(3, 'days'),
@@ -370,4 +369,5 @@ function viewMemory(){
 			    }); 
 		    })
 }
+
 
