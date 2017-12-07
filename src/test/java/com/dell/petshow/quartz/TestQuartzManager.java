9b507c1a1b2259f14c1e49605a1381ba29f74a3d
Ma <Paul.P.Ma@emc.com>
2017-12-07 17:14:23 +0800

    ignore this

--- src/test/java/com/dell/petshow/quartz/TestQuartzManager.java
+++ src/test/java/com/dell/petshow/quartz/TestQuartzManager.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:spring/applicationContext-dao.xml", "classpath:spring/applicationContext-quartz.xml"})
+@ContextConfiguration({"classpath:spring/applicationContext-dao.xml"})
 public class TestQuartzManager {
 
 
