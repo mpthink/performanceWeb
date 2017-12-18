7afb1d5ad46ab53b265c77d69da27cddc1cdcc6f
Ma <Paul.P.Ma@emc.com>
2017-12-18 17:13:42 +0800

    add CPU data

--- src/main/webapp/WEB-INF/static/my/vtas/cpu.js
+++ src/main/webapp/WEB-INF/static/my/vtas/cpu.js
@@ -150,9 +150,21 @@ function viewMemory(){
 			 Chart.showLoading({text: 'Loding...'});  
 			 var xAxisData;
 			 var SPA_CPU_FILT=[];
+			 var SPA_cpu_PEService_exe=[];
+			 var SPA_cpu_csx_ic_safe=[];
+			 var SPA_cpu_ECOM=[];
+			 var SPA_cpu_mozzo_sh=[];
+			 var SPA_cpu_tomcat_sh=[];
+			 var SPA_cpu_TLDlistener_exe=[];
 			 
 			 var SPB_CPU_FILT=[];
-
+			 var SPB_cpu_PEService_exe=[];
+			 var SPB_cpu_csx_ic_safe=[];
+			 var SPB_cpu_ECOM=[];
+			 var SPB_cpu_mozzo_sh=[];
+			 var SPB_cpu_tomcat_sh=[];
+			 var SPB_cpu_TLDlistener_exe=[];
+			 
 			 var times=[];
 			 $.ajax({  
 			        url:dataUrl,  
@@ -168,9 +180,22 @@ function viewMemory(){
 					 				
 						 			times.push(result[i]['poll_datetime']);
 						 			SPA_CPU_FILT.push(result[i]['CPU_FILT']);
+						 			SPA_cpu_PEService_exe.push(result[i]['cpu_PEService_exe']);
+						 			SPA_cpu_csx_ic_safe.push(result[i]['cpu_csx_ic_safe']);
+						 			SPA_cpu_ECOM.push(result[i]['cpu_ECOM']);
+						 			SPA_cpu_mozzo_sh.push(result[i]['cpu_mozzo_sh']);
+						 			SPA_cpu_tomcat_sh.push(result[i]['cpu_tomcat_sh']);
+						 			SPA_cpu_TLDlistener_exe.push(result[i]['cpu_TLDlistener_exe']);
+						 			
 					 			}else if(result[i]['sp']=='SPB'){
 					 				
 					 				SPB_CPU_FILT.push(result[i]['CPU_FILT']);
+					 				SPB_cpu_PEService_exe.push(result[i]['cpu_PEService_exe']);
+					 				SPB_cpu_csx_ic_safe.push(result[i]['cpu_csx_ic_safe']);
+					 				SPB_cpu_ECOM.push(result[i]['cpu_ECOM']);
+					 				SPB_cpu_mozzo_sh.push(result[i]['cpu_mozzo_sh']);
+					 				SPB_cpu_tomcat_sh.push(result[i]['cpu_tomcat_sh']);
+					 				SPB_cpu_TLDlistener_exe.push(result[i]['cpu_TLDlistener_exe']);
 					 			}
 					 		 }
 					 	}
@@ -193,9 +218,20 @@ function viewMemory(){
 					                	selected: {
 					                		'SPA_CPU_FILT' : true,
 					                		'SPB_CPU_FILT' : true,
-
+					                		'SPA_PEService' : false,
+					                		'SPA_csx_ic_safe' : false,
+					                		'SPA_ECOM' : false,
+					                		'SPA_mozzo_sh' : false,
+					                		'SPA_tomcat_sh' : false,
+					                		'SPA_TLDlistener' : false,
+					                		'SPB_PEService' : false,
+					                		'SPB_csx_ic_safe' : false,
+					                		'SPB_ECOM' : false,
+					                		'SPB_mozzo_sh' : false,
+					                		'SPB_tomcat_sh' : false,
+					                		'SPB_TLDlistener' : false,
 					                	},
-					                	data: ['SPA_CPU','','SPB_CPU']
+					                	data: ['SPA_CPU','SPA_PEService','SPA_csx_ic_safe','SPA_ECOM','SPA_mozzo_sh','SPA_tomcat_sh','SPA_TLDlistener','','SPB_CPU','SPB_PEService','SPB_csx_ic_safe','SPB_ECOM','SPB_mozzo_sh','SPB_tomcat_sh','SPB_TLDlistener']
 					                },
 					                toolbox: {
 					    				show : false,
@@ -228,7 +264,7 @@ function viewMemory(){
 					                    }    
 					                ],    
 					                series: [
-					                	{
+					                		{
 					                		'name': 'SPA_CPU',    
 					                		'type': 'line',
 					                		'smooth':true,
@@ -236,12 +272,97 @@ function viewMemory(){
 					                		'data': SPA_CPU_FILT    
 					                		},
 					                		{
-					                		'name': 'SPB_CPU',    
-					                		'type': 'line',
-					                		'smooth':true,
-					                		'symbol':'none',
-					                		'data': SPB_CPU_FILT    
-					                		}
+						                		'name': 'SPA_PEService',    
+						                		'type': 'line',
+						                		'smooth':true,
+						                		'symbol':'none',
+						                		'data': SPA_cpu_PEService_exe    
+						                	},
+					                		{
+						                		'name': 'SPA_csx_ic_safe',    
+						                		'type': 'line',
+						                		'smooth':true,
+						                		'symbol':'none',
+						                		'data': SPA_cpu_csx_ic_safe    
+						                	},
+					                		{
+						                		'name': 'SPA_ECOM',    
+						                		'type': 'line',
+						                		'smooth':true,
+						                		'symbol':'none',
+						                		'data': SPA_cpu_ECOM    
+						                	},
+					                		{
+						                		'name': 'SPA_mozzo_sh',    
+						                		'type': 'line',
+						                		'smooth':true,
+						                		'symbol':'none',
+						                		'data': SPA_cpu_mozzo_sh    
+						                	},
+					                		{
+						                		'name': 'SPA_tomcat_sh',    
+						                		'type': 'line',
+						                		'smooth':true,
+						                		'symbol':'none',
+						                		'data': SPA_cpu_tomcat_sh    
+						                	},
+					                		{
+						                		'name': 'SPA_TLDlistener',    
+						                		'type': 'line',
+						                		'smooth':true,
+						                		'symbol':'none',
+						                		'data': SPA_cpu_TLDlistener_exe    
+						                	},
+						                	
+					                		{
+						                		'name': 'SPB_CPU',    
+						                		'type': 'line',
+						                		'smooth':true,
+						                		'symbol':'none',
+						                		'data': SPB_CPU_FILT    
+					                		},
+						                	{
+						                		'name': 'SPB_PEService',    
+						                		'type': 'line',
+						                		'smooth':true,
+						                		'symbol':'none',
+						                		'data': SPB_cpu_PEService_exe
+						                	},
+					                		{
+						                		'name': 'SPB_csx_ic_safe',    
+						                		'type': 'line',
+						                		'smooth':true,
+						                		'symbol':'none',
+						                		'data': SPB_cpu_csx_ic_safe    
+						                	},
+					                		{
+						                		'name': 'SPB_ECOM',    
+						                		'type': 'line',
+						                		'smooth':true,
+						                		'symbol':'none',
+						                		'data': SPB_cpu_ECOM    
+						                	},
+					                		{
+						                		'name': 'SPB_mozzo_sh',    
+						                		'type': 'line',
+						                		'smooth':true,
+						                		'symbol':'none',
+						                		'data': SPB_cpu_mozzo_sh    
+						                	},
+					                		{
+						                		'name': 'SPB_tomcat_sh',    
+						                		'type': 'line',
+						                		'smooth':true,
+						                		'symbol':'none',
+						                		'data': SPB_cpu_tomcat_sh    
+						                	},
+					                		{
+						                		'name': 'SPB_TLDlistener',    
+						                		'type': 'line',
+						                		'smooth':true,
+						                		'symbol':'none',
+						                		'data': SPB_cpu_TLDlistener_exe    
+						                	}
 					                ]
 					            };
 						
